f3f82a98fa0e28e4dc87278c55b78afea7de2672
MChristian00 <manzichristian07@gmail.com>
2023-03-15 00:25:14 -0400

    Changed some codes

--- Projects/Project 1/SubstitutionCipher.java
+++ Projects/Project 1/SubstitutionCipher.java
@@ -1,32 +1,43 @@
-package Projects.Project 1;
+// package Projects.Project 1;
 
 import java.util.*;
 
-public class SubstitutionCipher extends Cipher{
-    
-Random rand = new Random(256);
-char[] randChars = new char[256];
+public class SubstitutionCipher extends Cipher {
 
-@Override
-public List<Character> encrypt(List<Character> cleartext){
-    // create random array of chars using char(rand number)
-    // loop through cleartext and get each charValue(char in cleartext) using foreach and retrieve 
-    //that value from random array and add it to string decryptedMsg
-
-    for(int i=255; i>0; i--){
-        randChars[i] = (char) rand.nextInt(i);
-    }
-    
-    for(char randChar : randChars){
-        System.out.print(randChar);
+    public SubstitutionCipher(long key) {
+        super(key);
+        // TODO Auto-generated constructor stub
     }
 
-}
-public List<Character> decrypt(List<Character> cleartext){}
+    Random rand = new Random(256);
+    List<Character> randChars = new ArrayList<>();
 
-public static void main(String[] args){
-for (int i = 0; i < 256; i ++){
-  System.out.println((char) i);
-}
-}
+    @Override
+    // public List<Character> encrypt(List<Character> cleartext){
+    public List<Character> encrypt(List<Character> clearText) {
+        // create random array of chars using char(rand number)
+        // loop through cleartext and get each charValue(char in cleartext) using
+        // foreach and retrieve
+        // that value from random array and add it to string decryptedMsg
+
+        for (int i = 255; i > 0; i--) {
+            randChars[i] = (char) rand.nextInt(i);
+        }
+
+        for (char randChar : clearText) {
+            System.out.print(randChar);
+        }
+        return clearText;
+    }
+    public List<Character> decrypt(List<Character> encryptedText){
+        return encryptedText;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 256; i++) {
+            System.out.println((char) i);
+        }
+        SubstitutionCipher s = new SubstitutionCipher(1);
+        s.encrypt(List);
+    }
 }
