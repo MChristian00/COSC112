ecef9b36e00431d816b3d3cca4d803982b34948b
MCHristian00 <manzichristian07@gmail.com>
2023-03-15 01:20:07 -0400

    Encrypts and decrypts some of the letters

--- Projects/Project 1/SubstitutionCipher.java
+++ Projects/Project 1/SubstitutionCipher.java
@@ -4,13 +4,22 @@ import java.util.*;
 
 public class SubstitutionCipher extends Cipher {
 
+    List<Character> randChars = new ArrayList<>();
+
     public SubstitutionCipher(long key) {
         super(key);
         // TODO Auto-generated constructor stub
-    }
 
-    Random rand = new Random(256);
-    List<Character> randChars = new ArrayList<>();
+        Random rand = new Random(key);
+        for (int i = 255; i > 0; i--) {
+            // char aChar = (char) rand.nextInt(i);
+            // if (Character.isLetter(aChar)) {
+            randChars.add((char) rand.nextInt(i));
+            // }
+        }
+
+        System.out.print(randChars);
+    }
 
     @Override
     // public List<Character> encrypt(List<Character> cleartext){
@@ -20,24 +29,55 @@ public class SubstitutionCipher extends Cipher {
         // foreach and retrieve
         // that value from random array and add it to string decryptedMsg
 
-        for (int i = 255; i > 0; i--) {
-            randChars[i] = (char) rand.nextInt(i);
-        }
+        List<Character> ciphertext = new ArrayList<>();
 
-        for (char randChar : clearText) {
-            System.out.print(randChar);
+        for (char aChar : clearText) {
+            // System.out.print(aChar);
+            // get index of aChar
+            // retrieve what's at index=index(achar)
+            // add it to ciphertext
+            ciphertext.add(randChars.get((int) aChar));
         }
-        return clearText;
+        System.out.print("Cipher Text" + ciphertext);
+        return ciphertext;
     }
-    public List<Character> decrypt(List<Character> encryptedText){
-        return encryptedText;
+
+    public List<Character> decrypt(List<Character> encryptedText) {
+        List<Character> originaltext = new ArrayList<>();
+
+        for (char aChar : encryptedText) {
+            // System.out.print(aChar);
+            // get index of aChar
+            // retrieve what's at (char) index(achar)
+            // add it to originaltext
+            originaltext.add((char) randChars.indexOf(aChar));
+        }
+        System.out.print("OG Text" + originaltext);
+        return originaltext;
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 256; i++) {
-            System.out.println((char) i);
-        }
-        SubstitutionCipher s = new SubstitutionCipher(1);
-        s.encrypt(List);
+        // for (int i = 0; i < 256; i++) {
+        // System.out.println((char) i);
+        // }
+        List<Character> clearSample = new ArrayList() {
+            {
+                add('H');
+                add('e');
+                add('l');
+                add('l');
+            }
+        };
+        List<Character> encryptSample = new ArrayList() {
+            {
+                add('?');
+                add('k');
+                add('U');
+                add('U');
+            }
+        };
+        SubstitutionCipher s = new SubstitutionCipher(20);
+        s.encrypt(clearSample);
+        s.decrypt(encryptSample);
     }
 }
