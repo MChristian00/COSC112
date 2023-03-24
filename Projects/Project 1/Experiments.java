d9d8693505a2e1a8c3ff2c1f3f524b11e818250b
MCHristian00 <manzichristian07@gmail.com>
2023-03-24 13:14:05 -0400

    Some more changes

--- Projects/Project 1/Experiments.java
+++ Projects/Project 1/Experiments.java
@@ -1,91 +1,103 @@
-import java.io.BufferedWriter;
+import java.io.PrintWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class Experiments {
 
-  static int[][] exp = new int[256][256];
+  static int[][] table = new int[256][256];
+  static List<Character> unShuffledChars = new ArrayList<>();
+  static List<Character> shuffledChars = new ArrayList<>();
   static int callCounter = 0;
   static int currentValue;
+
+  public static List<Character> generateUnShuffledArray() {
+    for (int i = 0; i < 256; i++) {
+      unShuffledChars.add((char) i);
+    }
+    return unShuffledChars;
+  }
+
   /**
-     * @param both encrypt and decrypt list of chars
-     * @return mapped freqs
-     */
-    public static void findFreqOfChar(List<Character> ogList, List<Character> encList) {
-      callCounter++;
-        for(int i = 0; i < ogList.size(); i++){
-            currentValue = exp[(int) ogList.get(i)][(int) encList.get(i)];
-            currentValue = (currentValue/100) + 1;
-            exp[(int) ogList.get(i)][(int) encList.get(i)] = (currentValue*100)/callCounter;
+   * @param key
+   * @return shuffledChars an array of randomly placed characters
+   */
+  public static List<Character> generateShuffledArray(long key) {
+    Random rand = new Random(key);
+    List<Character> copyArray = unShuffledChars;
+    while (copyArray.size() != 0) {
+      char randChar = (char) rand.nextInt(copyArray.size());
+      shuffledChars.add(randChar);
+      copyArray.remove(randChar);
+    }
+    return shuffledChars;
+  }
+
+  /**
+   * @param key
+   */
+  public static void populateArrays(long key) {
+    generateUnShuffledArray();
+    generateShuffledArray(key);
+  }
+
+  /**
+   * @param key
+   */
+  public static void findFreqOfChar(long key) {
+    generateUnShuffledArray();
+    callCounter++;
+    for (int i = 0; i < unShuffledChars.size(); i++) {
+      currentValue = table[(int) unShuffledChars.get(i)][(int) shuffledChars.get(i)];
+      currentValue = (currentValue / 100) + 1;
+      table[(int) unShuffledChars.get(i)][(int) shuffledChars.get(i)] = (currentValue * 100) / callCounter;
+    }
+  }
+
+  public static void addHeaders() {
+    for (int i = 0; i < 256; i++) {
+      for (int j = 0; j < 256; j++) {
+        if (i == 0 && j == 0) {
+          continue;
         }
+        if (i == 0) {
+          table[i][j] = j - 1;
+          table[j][i] = j - 1;
+        }
+      }
+    }
+  }
+
+  public static void writeToFile() throws IOException {
+    File f = new File("./Table.txt");
+    PrintWriter pw = new PrintWriter(f);
+
+    if (!f.exists()) {
+      f.createNewFile();
     }
 
-
-    public static void populateTable() {
-        for(int i=0; i<256; i++){
-          for(int j=0; j<256; j++){
-            if(i == 0 && j == 0){
-              continue;
-            }
-            if(i==0){
-                exp[i][j] = j-1;
-                exp[j][i] = j-1;
-            }
-            }
-          }
-          
-        }
-
-    public static void writeToFile(List<Character> ogList, List<Character> encList) throws IOException{
-        File f = new File("./Table.txt");
-        if(!f.exists()) {
-          f.createNewFile();
-        }
-
-        FileWriter fw = new FileWriter(f.getAbsoluteFile());
-
-        BufferedWriter bw = new BufferedWriter(fw);
-        
-        
-          for(int i=0; i<256; i++){
-            for(int j=0; j<256; j++){
-              bw.write(exp[i][j]);
-              
-              }
-            }
-
-      bw.close();
-      fw.close();
-
+    for (int i = 0; i < 256; i++) {
+      for (int j = 0; j < 256; j++) {
+        pw.write(table[i][j] + "     ");
+      }
+      pw.write("\n");
     }
-
-  
+    pw.close();
+  }
 
   public static void main(String[] args) throws IOException {
-    List<Character> ogText = new ArrayList<>() {
-            {
-                add('');
-                add('');
-
-            }
-        };
-    List<Character> encText = new ArrayList<>() {
-            {
-                add('');
-                add('');
-            }
-        };
-
-      Experiments.populateTable();
-      Experiments.findFreqOfChar(ogText, encText);
-      Experiments.findFreqOfChar(ogText, encText);
-      Experiments.findFreqOfChar(ogText, encText);
-      Experiments.writeToFile(ogText, encText);
-      for(int i=0; i<40; i++){
-          for(int j=0; j<40; j++){
-            System.out.printf("%d ",Experiments.exp[i][j]);
-          } System.out.println();}  
+    // Experiments a = new Experiments(3);
+    Experiments.populateArrays(12);
+    Experiments.addHeaders();
+    Experiments.findFreqOfChar(12);
+    Experiments.findFreqOfChar(12);
+    // Experiments.findFreqOfChar(5);
+    Experiments.writeToFile();
+    for (int i = 0; i < 256; i++) {
+      for (int j = 0; j < 256; j++) {
+        System.out.printf("%d ", Experiments.table[i][j]);
+      }
+      System.out.println();
+    }
   }
 }
--- Projects/Project 1/SubstitutionCipher.java
+++ Projects/Project 1/SubstitutionCipher.java
@@ -3,14 +3,12 @@
 import java.util.*;
 
 public class SubstitutionCipher extends Cipher {
-  
-    List<Character> charToFreqMap = new ArrayList<>();
+
     List<Character> allChars = new ArrayList<>();
     List<Character> randChars = new ArrayList<>();
 
     public SubstitutionCipher(long key) {
         super(key);
-        // TODO Auto-generated constructor stub
 
         for (int i = 0; i < 256; i++) {
             allChars.add((char) i);
@@ -24,85 +22,23 @@ public class SubstitutionCipher extends Cipher {
         }
     }
 
+    @Override
+    public List<Character> encrypt(List<Character> originalText) {
+        List<Character> encryptedText = new ArrayList<>();
 
-    /**
-     * @param listOfChars
-     * @return table of char and their usage frequencies
-     */
-    // public void findRandFreq(List<Character> clearListOfChars, List<Character>
-    // randListOfChars) {
-    public void findRandFreq(List<Character> clearListOfChars) {
-        // int count = 0;
-        for (char aChar : clearListOfChars) {
-            int randCharFreq = Collections.frequency(clearListOfChars, aChar);
-            System.out.printf("%c  appears %d times \n", aChar, randCharFreq);
+        for (char aChar : originalText) {
+            encryptedText.add(randChars.get((int) aChar));
         }
-    }
-
-    /**
-     * @param both encrypt and decrypt list of chars
-     * @return mapped freqs
-     */
-    public void findFreqOfChar(List<Character> clearListOfChars) {
-        for (char aChar : clearListOfChars) {
-            int randCharFreq = Collections.frequency(clearListOfChars, aChar);
-            System.out.printf("%c  is mapped to %c - %d times \n", aChar, randCharFreq);
-        }
+        return encryptedText;
     }
 
     @Override
-    // public List<Character> encrypt(List<Character> cleartext){
-    public List<Character> encrypt(List<Character> clearText) {
-        // create random array of chars using char(rand number)
-        // loop through cleartext and get each charValue(char in cleartext) using
-        // foreach and retrieve
-        // that value from random array and add it to string decryptedMsg
-
-        List<Character> ciphertext = new ArrayList<>();
-
-        for (char aChar : clearText) {
-            // System.out.print(aChar);
-            // get index of aChar
-            // retrieve what's at index=index(achar)
-            // add it to ciphertext
-            ciphertext.add(randChars.get((int) aChar));
-        }
-        // System.out.print("Cipher Text" + ciphertext);
-        return ciphertext;
-    }
-
     public List<Character> decrypt(List<Character> encryptedText) {
         List<Character> originaltext = new ArrayList<>();
-        // System.out.println(encryptedText);
         for (char aChar : encryptedText) {
-            // System.out.print(aChar);
-            // get index of aChar
-            // retrieve what's at (char) index(achar)
-            // add it to originaltext
             originaltext.add((char) randChars.indexOf(aChar));
         }
-        // System.out.print("OG Text" + originaltext);
         return originaltext;
-    }
-
-    public static void main(String[] args) {
-        // for (int i = 0; i < 256; i++) {
-        // System.out.println((char) i);
-        // }
-        List<Character> clearSample = new ArrayList() {
-            {
-                add('H');
-                add('o');
-                add('b');
-                add('e');
-            }
-        };
-        SubstitutionCipher s = new SubstitutionCipher(19);
-        // SubstitutionCipher c = new SubstitutionCipher(19);
-        List<Character> encryptSample = s.encrypt(clearSample);
-        System.out.println(s.decrypt(encryptSample));
-        // s.findRandFreq(encryptSample);
-        // c.findRandFreq(encryptSample);
     }
 }
 
@@ -117,11 +53,15 @@ public class SubstitutionCipher extends Cipher {
  * ASK PROF: What are we limited to using? Just simple core java, or we can
  * import * libraries for instance the Joiner method that turns list of chars to
  * str. * Imported * from com.google.common.base.Joiner
+ * 
+ * Can I make Experiments.java going to be ran on it's own or is it to
+ * contain an interface? Or is it supposed to be called
+ * through the SubsitutionCipher, in that case, it has to be an interface since
+ * SC already extends 1 class (Cipher).
  */
 
-
 /**
-* Code to run:
-* java Crypt Substitution encrypt 17 < og.txt > enc.txt 
-* java Crypt Substitution decrypt 17 < enc.txt > og2.txt
-*/
+ * Code to run:
+ * java Crypt Substitution encrypt 17 < og.txt > enc.txt
+ * java Crypt Substitution decrypt 17 < enc.txt > og2.txt
+ */
