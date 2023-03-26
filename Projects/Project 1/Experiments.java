08d41a6cae0f3a924ee0173eb557b3049221d95e
MCHristian00 <manzichristian07@gmail.com>
2023-03-26 15:33:02 -0400

    Finalized everything and all works

--- Projects/Project 1/Experiments.java
+++ Projects/Project 1/Experiments.java
@@ -26,9 +26,10 @@ public class Experiments {
     Random rand = new Random(key);
     List<Character> copyArray = unShuffledChars;
     while (copyArray.size() != 0) {
-      char randChar = (char) rand.nextInt(copyArray.size());
+      int randCharIndex = rand.nextInt(copyArray.size());
+      char randChar = (char) randCharIndex;
       shuffledChars.add(randChar);
-      copyArray.remove(randChar);
+      copyArray.remove(randCharIndex);
     }
     return shuffledChars;
   }
@@ -44,10 +45,12 @@ public class Experiments {
   /**
    * @param key
    */
-  public static void findFreqOfChar(long key) {
-    generateUnShuffledArray();
+  public static void findFreqOfCharMapping(long key) {
+    if (callCounter == 0) {
+      generateUnShuffledArray();
+    }
     callCounter++;
-    for (int i = 0; i < unShuffledChars.size(); i++) {
+    for (int i = 0; i < unShuffledChars.size() - 1; i++) {
       currentValue = table[(int) unShuffledChars.get(i)][(int) shuffledChars.get(i)];
       currentValue = (currentValue / 100) + 1;
       table[(int) unShuffledChars.get(i)][(int) shuffledChars.get(i)] = (currentValue * 100) / callCounter;
@@ -60,7 +63,7 @@ public class Experiments {
         if (i == 0 && j == 0) {
           continue;
         }
-        if (i == 0) {
+        if (i == 0 || j == 1) {
           table[i][j] = j - 1;
           table[j][i] = j - 1;
         }
@@ -68,6 +71,9 @@ public class Experiments {
     }
   }
 
+  /**
+   * 
+   */
   public static void writeToFile() throws IOException {
     File f = new File("./Table.txt");
     PrintWriter pw = new PrintWriter(f);
@@ -78,26 +84,31 @@ public class Experiments {
 
     for (int i = 0; i < 256; i++) {
       for (int j = 0; j < 256; j++) {
-        pw.write(table[i][j] + "     ");
+        String s = String.format("%-7d", table[i][j]);
+        pw.write(s);
       }
       pw.write("\n");
     }
     pw.close();
   }
 
+  public static void description() {
+    String summary = """
+        I first generated two Linked lists, unShuffledChars and shuffledChars using a call to their corresponding methods from the constructor. I assigned all the first 256 characters to the unShuffledChars (starting at 0 to 255). To generate the shuffled characters array (shuffledChars), I used a user-defined random key to generate a random number that I used to get a character from a copy list (copyArray) of the unShuffledChars. I then added the character to unShuffledChars and deleted it from the copyArray. This generates two equal-sized lists of characters that I use in encryption and decryption.
+
+        To encrypt, I took the given list of characters and looped through the characters getting their corresponding index from the unShuffledChars and then using that index to get the random character at the same index from shuffledChars. For decrypting, I got the index of each character in the encryption text and used it to find the original character at the same index from the unShuffledChars list.
+          """;
+
+    System.out.print(summary);
+  }
+
   public static void main(String[] args) throws IOException {
-    // Experiments a = new Experiments(3);
     Experiments.populateArrays(12);
     Experiments.addHeaders();
-    Experiments.findFreqOfChar(12);
-    Experiments.findFreqOfChar(12);
-    // Experiments.findFreqOfChar(5);
-    Experiments.writeToFile();
-    for (int i = 0; i < 256; i++) {
-      for (int j = 0; j < 256; j++) {
-        System.out.printf("%d ", Experiments.table[i][j]);
-      }
-      System.out.println();
+    for (int i = 1; i <= 50; i++) {
+      Experiments.findFreqOfCharMapping(i);
     }
+    Experiments.writeToFile();
+    Experiments.description();
   }
 }
--- Projects/Project 1/SubstitutionCipher.java
+++ Projects/Project 1/SubstitutionCipher.java
@@ -1,33 +1,46 @@
-// package Projects.Project 1;
-
 import java.util.*;
 
 public class SubstitutionCipher extends Cipher {
 
-    List<Character> allChars = new ArrayList<>();
-    List<Character> randChars = new ArrayList<>();
+    List<Character> unShuffledChars = new ArrayList<>();
+    List<Character> shuffledChars = new ArrayList<>();
+    int index;
 
     public SubstitutionCipher(long key) {
         super(key);
+        generateUnShuffledChars();
+        generateShuffledChars(key);
+        generateUnShuffledChars();
+    }
 
+    public void generateUnShuffledChars() {
         for (int i = 0; i < 256; i++) {
-            allChars.add((char) i);
+            unShuffledChars.add((char) i);
         }
+    }
 
+    /**
+     * @param key
+     * @return shuffledChars an array of randomly placed characters
+     */
+    public void generateShuffledChars(long key) {
         Random rand = new Random(key);
-        while (allChars.size() != 0) {
-            char randChar = (char) rand.nextInt(allChars.size());
-            randChars.add(randChar);
-            allChars.remove(randChar);
+        List<Character> copyArray = unShuffledChars;
+        while (copyArray.size() != 0) {
+            int randCharIndex = rand.nextInt(copyArray.size());
+            char randChar = copyArray.get(randCharIndex);
+            shuffledChars.add(randChar);
+            copyArray.remove(copyArray.indexOf(randChar));
         }
     }
 
     @Override
     public List<Character> encrypt(List<Character> originalText) {
-        List<Character> encryptedText = new ArrayList<>();
 
+        List<Character> encryptedText = new ArrayList<>();
         for (char aChar : originalText) {
-            encryptedText.add(randChars.get((int) aChar));
+            index = unShuffledChars.indexOf(aChar);
+            encryptedText.add(shuffledChars.get(index));
         }
         return encryptedText;
     }
@@ -36,32 +49,9 @@ public class SubstitutionCipher extends Cipher {
     public List<Character> decrypt(List<Character> encryptedText) {
         List<Character> originaltext = new ArrayList<>();
         for (char aChar : encryptedText) {
-            originaltext.add((char) randChars.indexOf(aChar));
+            index = shuffledChars.indexOf(aChar);
+            originaltext.add(unShuffledChars.get(index));
         }
         return originaltext;
     }
 }
-
-/**
- * Problems: If I don't change the key, and I change the OG Text,
- * if I use the previous cipher Text, then it will return the previous OG Text
- * 
- * Also, some characters are not displayed but using copy-n-paste into my code,
- * works eventho I can't see anything
- * 
- * 
- * ASK PROF: What are we limited to using? Just simple core java, or we can
- * import * libraries for instance the Joiner method that turns list of chars to
- * str. * Imported * from com.google.common.base.Joiner
- * 
- * Can I make Experiments.java going to be ran on it's own or is it to
- * contain an interface? Or is it supposed to be called
- * through the SubsitutionCipher, in that case, it has to be an interface since
- * SC already extends 1 class (Cipher).
- */
-
-/**
- * Code to run:
- * java Crypt Substitution encrypt 17 < og.txt > enc.txt
- * java Crypt Substitution decrypt 17 < enc.txt > og2.txt
- */
