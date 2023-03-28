eaa425ca9647eeb2cfc4d536c9fe9beb1611868f
MCHristian00 <manzichristian07@gmail.com>
2023-03-27 23:35:40 -0400

    Initial classes

---
+++ Projects/Project_2/Pong.java
@@ -0,0 +1,31 @@
+package PROJECTS.Project_2;
+
+public class Pong {
+    public Pong() {
+
+    }
+
+    class Pair {
+
+    }
+
+    class Ball {
+
+    }
+
+    class Paddle {
+
+    }
+
+    class World {
+
+    }
+
+    class KeyBoard {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
