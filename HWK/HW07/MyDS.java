ca3054e04b037fb97a9847624d0f096187aadbe9
MCHristian00 <manzichristian07@gmail.com>
2023-04-02 01:20:38 -0400

    Init files for HWK7

---
+++ HWK/HW07/MyDS.java
@@ -0,0 +1,7 @@
+public class MyDS extends OrderedCollection{
+    public void append(int toAppend){}
+    public int peek(){}
+    public int pop(){}
+    public String toString(){}
+    public int length(){}
+}
---
+++ HWK/HW07/Tester.java
@@ -0,0 +1,9 @@
+public class Tester{
+    public static void main(String args[]){
+        MyDS ds = new MyDS();
+        testDS(ds);
+    }
+    public static void testDS(OrderedCollection ds){
+      //append some ints to ds, pop from ds, peek, and print it
+    }
+}
