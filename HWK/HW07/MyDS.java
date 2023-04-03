c4c2b2d7881e57f69b8d69444782b6036d499f8e
Christian Manzi <56400182+MChristian00@users.noreply.github.com>
2023-04-03 02:29:48 +0000

    Implemented linkedlist data structure complete

--- HWK/HW07/MyDS.java
+++ HWK/HW07/MyDS.java
@@ -1,7 +1,61 @@
 public class MyDS extends OrderedCollection{
-    public void append(int toAppend){}
-    public int peek(){}
-    public int pop(){}
-    public String toString(){}
-    public int length(){}
+    Node end;
+
+    public MyDS(){
+        super();
+        end = null;
+    }
+
+    class Node{
+        Node prev;
+        int n;
+        public Node(int n){
+            this.n = n;
+        }
+    }
+
+    public void append(int toAppend){
+        Node newN = new Node(toAppend);
+        newN.prev = end;
+        end = newN;
+    }
+
+    public int peek(){
+        if(end == null){
+            return 0;
+        }
+        return end.n;
+    }
+
+    public int pop(){
+        Node currentEnd;
+        if(end == null){
+            return 0;
+        }
+        currentEnd = end;
+        end = end.prev;
+        return currentEnd.n;
+    }
+
+    public String toString(){
+        String toReturn="";
+        Node intH;
+        intH = end;
+        while(intH != null){
+            toReturn += intH.n;
+            intH = intH.prev;
+        }
+        return toReturn;
+    }
+
+    public int length(){
+        int count = 0;
+        Node n;
+        n = end;
+        while(n != null){
+            count++;
+            n = n.prev;
+        }
+        return count;
+    }
 }
--- HWK/HW07/Tester.java
+++ HWK/HW07/Tester.java
@@ -5,5 +5,14 @@ public class Tester{
     }
     public static void testDS(OrderedCollection ds){
       //append some ints to ds, pop from ds, peek, and print it
+
+      ds.append(7);
+      ds.append(2);
+      ds.append(712);
+      ds.append(12);
+      System.out.println(ds.toString());
+      System.out.println(ds.peek());
+      System.out.println(ds.pop());
+      System.out.println(ds.length());
     }
 }
